f825ecaf361ec0572b798cf9e0f8e67e09ca8c93
mark-elias <mark7elias@gmail.com>
2024-08-22 02:37:36 -0700

    basic index.js created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+//=====================================
+//middleware so that we can use json
+app.use(express.json());
+
+//=====================================
+const port = process.env.PORT || 5002;
+app.listen(port, () => {
+  console.log(`🦻 Listening on Port: ${port}...`);
+});
